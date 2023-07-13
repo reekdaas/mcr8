39d51976675a085fca230f9e12c4119f086f08ab
Rittik Das <dassrittik1998@gmail.com>
2023-07-13 21:50:14 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
 import "./App.css";
 import AppRouter from "./AppRouter";
+import { data } from "./data/data";
+import Header from "./component/header/header";
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-      <NavLink to="/">Home</NavLink>
+      <Header />
       <AppRouter />
     </div>
   );
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import EventPage from "./pages/eventPage/eventPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/:id" element={<EventPage />} />
     </Routes>
   );
 }
